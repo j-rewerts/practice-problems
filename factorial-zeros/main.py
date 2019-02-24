39118002502279fd7b4c3527391877a45f5d8d32
Jared Rewerts <jaredrewerts@gmail.com>
2019-02-24 12:55:17 -0700

    Added the run time.

--- factorial-zeros/main.py
+++ factorial-zeros/main.py
@@ -1,8 +1,9 @@
 # Cracking the Coding Interview, Question 16.5
 # @author CodyGramlich and j-rewerts
 # Calculates the number of zeros in n factorial.
+import math
 
-
+# Run time O(logn)
 def howManyFives(num):
   fives = 0
   while num % 5 == 0:
@@ -10,10 +11,11 @@ def howManyFives(num):
     fives += 1
   return fives
 
+# Runs in O(n logn)
 def factZeros(n):
   fives = 0
   for i in range(5, n+1, 5):
     fives += howManyFives(i)
   return fives
 
-print(factZeros(125))
+print(factZeros(1000000000))

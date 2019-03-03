6562dead4c7f2c429fb69f35db2e7d227207b703
CodyGramlich <cody.gramlich@edmonton.ca>
2019-03-03 10:06:23 -0700

    Added Node

--- LRU-16.25/main.py
+++ LRU-16.25/main.py
@@ -1,15 +1,26 @@
 from queue import Queue
 
+class Node():
+  def __init__(self, value):
+    self.value = value
+    self.next = Node(None)
+  
+
 class LRU():
   def __init__(self, maxSize):
     self.maxSize = maxSize
     self.dict = {}
-    self.q = Queue()
+    self.tail = None
+    self.head = None
 
-  def get(key):
+  def get(self, key):
     pass
   
-  def insert(key, value):
-    pass
+  def insert(self, key, value):
+    node = Node(value)
+    if len(self.dict) == 0:
+      self.dict[key] = node
+      self.tail = node
+      self.head = node
 
 myCache = LRU(4)

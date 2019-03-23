aac933d54c8dc66ce1c02cf9bb735c1201c30576
Jared Rewerts <jaredrewerts@gmail.com>
2019-03-23 17:05:40 -0600

    Added YAML parsing for running benchmarks.

---
+++ bench.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import yaml
+
+FILE = './benchmarks.yml'
+
+with open(FILE, 'r') as stream:
+  try:
+    config = yaml.safe_load(stream)
+  except yaml.YAMLError as exc:
+    print(exc)
+
+for test in config['tests']:
+  print(test['name'])
+  for run in test['work']:
+    print('Running ' + run['name'])
